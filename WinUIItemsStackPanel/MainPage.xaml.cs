95b6f54eb900b90ec82b521ff05cfe1d9f0ea938
Ponant <ponant@yombo.com>
2020-01-23 10:46:43 +0100

    Add project files.

---
+++ WinUIItemsStackPanel/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WinUIItemsStackPanel
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Items = new ObservableCollection<string>();
+            for (int i = 0; i < 30000; i++)//Increase length here
+            {
+                Items.Add($"Item {i}");
+            }
+        }
+        public ObservableCollection<string> Items { get; }
+    }
+}
